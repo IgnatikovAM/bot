702d52cf0949fda86237d7cbfbe3c9066bc1e1c6
IgnatikovAM <89780074+IgnatikovAM@users.noreply.github.com>
2025-05-10 21:32:47 +0300

    Create constants.js

---
+++ src/constants.js
@@ -0,0 +1,6 @@
+export const serviceTypes = new Set([
+  'notification','notification_template','call_log',
+  'revoked','e2e_notification','protocol'
+]);
+
+export const allowedTypes = new Set(['text','voice','audio','music']);
